6eaae666e3d5e2d117a973b66e86d458925e8f5f
Gustav Delius <gustav.delius@gmail.com>
2024-10-23 22:44:17 +0100

    Plot density instead of counts

--- plot_catch.R
+++ plot_catch.R
@@ -1,18 +1,24 @@
 # Plot observed catches against modelled catches
 plot_catch <- function(params, catch) {
+    heights <- catch$count / catch$dl / sum(catch$count)
 
-    hist_data <- data.frame(
-        length = catch$length + catch$dl / 2,
-        count = catch$count
-    )
-    plot(hist_data$length, hist_data$count, type = 'h', lwd = 2, col = 'blue',
-         xlab = 'Length', ylab = 'Count', main = 'Catch size distribution')
+    # Create an empty plot with the correct x and y limits
+    plot(NULL, xlim = c(min(catch$length), max(catch$length + catch$dl)),
+         ylim = c(0, max(heights)), xlab = "Length [cm]", ylab = "Density",
+         main = "Histogram with Areas Representing Counts")
 
-    model_catch <- params@initial_n * getFMort(params) * params@dw
-    # Scale the catch for visualization
-    model_catch <- model_catch * max(hist_data$count) / max(model_catch)
+    # Draw the bars using rect()
+    rect(catch$length, 0, catch$length + catch$dl, heights,
+         col = "blue", border = "blue")
+
     # We use lengths instead of weights in the catch data
     lengths <- (params@w / params@species_params$a)^(1/params@species_params$b)
+    # Catch density as a function of weight
+    model_catch <- params@initial_n * getFMort(params)
+    # Normalise the density
+    model_catch <- model_catch  / sum(model_catch * params@dw)
+    # Convert to density as a function of length
+    model_catch <- model_catch * params@species_params$b * params@w / lengths
 
     # Add the fitted PDF to the plot
     lines(lengths, model_catch, col = 'red', lwd = 2)
